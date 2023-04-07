257f44174aa34d94dfc924a08e5426c6aa6f6a9b
Alex Samaroo <alexsamaroo32@gmail.com>
2023-04-07 16:40:56 -0400

    Edited branch.py and employee.py

--- code/branch.py
+++ code/branch.py
@@ -22,3 +22,47 @@ async def getBranches():
 #Todo: add more requests for branches such as a GET request to get a particular branch
 #      perhaps a POST request to create a new branch, a DELETE request to remove a branch
 #      and a PUT request to update a branch (maybe something like update the cash available)
+
+#GET request to get a particular branch specified by Branch ID
+@app.get("/branches/{branch_id}")
+async def getBranchByID(branch_id: int):
+    branch = db_session.query(BankTable). \
+        filter(BankTable.branch_id == branch_id).first()
+    return branch
+
+#POST request to create a branch with a new Branch ID
+@app.post("/branches")
+async def createBranch(id: int):
+    bank = BankTable()
+    bank.branch_id = id
+    #ADD MORE VARIABLES
+    db_session.add(bank)
+    db_session.commit()
+    return {"Great Success!": True}
+
+
+#DELETE request to delete a branch
+@app.delete("/branches/{branch_id}")
+async def deleteBranch(branch_id: int):
+    branch = db_session.query(BankTable). \
+        filter(BankTable.id == branch_id).first()
+
+    if not branch:
+        raise HTTPException(status_code=404, detail="Branch not found")
+
+    db_session.delete(branch)
+    db_session.commit()
+    return {"Great Success!": True}
+
+#PUT request to update the amount of cash avaiable at a branch
+@app.put("/branches/{branch_id}")
+async def updateBranch(branch_id: int, amount: int):
+    branch = db_session.query(BankTable). \
+        filter(BankTable.branch_id == branch_id).first()
+    if not branch:
+        raise HTTPException(status_code=404, detail="Branch not found")
+    branch.amount = amount
+
+    db_session.commit()
+
+    return {"Great Success": True}
--- code/employee.py
+++ code/employee.py
@@ -16,8 +16,54 @@ app.add_middleware(
 
 
 #GET request that returns list of all employees
-@app.get("/patients")
-def getEmployees():
+@app.get("/employees")
+async def getEmployees():
     return db_session.query(EmployeeTable).all()
 
 #Todo: create requests to Create, Read, Update, and Delete (CRUD) employees
+
+#GET request to get employee by ID number
+@app.get("/employees/{employee_id}")
+async def getEmployeeByID(employee_id: int):
+    employee = db_session.query(EmployeeTable). \
+        filter(EmployeeTable.employee_id == employee_id).first()
+    return employee
+
+
+#POST request to create a new employee
+@app.post("/employees")  # create a new employee
+async def createEmployee(id: int, employee_id: int):
+    employee = EmployeeTable()
+    employee.employee_id = id
+    employee.employee_id = employee_id
+    db_session.add(employee)
+    db_session.commit()
+    return {"Success": True}
+
+
+#DELETE request to delete an employee specified by ID
+@app.delete("/employees/{employee_id}")  # discharge a employee
+async def deleteEmployee(employee_id: int):
+    print("employee_ID RECIEVED" + str(employee_id))
+    employee = db_session.query(EmployeeTable). \
+        filter(EmployeeTable.id == employee_id).first()
+
+    if not employee:
+        raise HTTPException(status_code=404, detail="Employee not found")
+
+    db_session.delete(employee)
+    db_session.commit()
+    return {"Success": True}
+
+#PUT request to update the employee ID of an employee
+@app.put("/employees/{employee_id}")
+async def updateemployee(employee_id: int, branch_id: int):
+    employee = db_session.query(EmployeeTable). \
+        filter(EmployeeTable.employee_id == employee_id).first()
+    if not employee:
+        raise HTTPException(status_code=404, detail="Employee not found")
+    employee.branch_id = branch_id
+
+    db_session.commit()
+
+    return {"Great Success": True}
